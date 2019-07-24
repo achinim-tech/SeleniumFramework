745396bd34335828a5da8b81ae0301e1f574979d
Achini <Piyadasa@Piyadasa-PC>
2019-07-24 13:08:18 +0530

    Selenium commit

---
+++ com.ocr.testing/src/test/java/com/ocr/testing/OCRTest.java
@@ -0,0 +1,39 @@
+package com.ocr.testing;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.awt.Image;
+import java.awt.image.RenderedImage;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class OCRTest {
+	
+	WebDriver driver;
+	   
+	 @BeforeTest  
+	  public void setUpDriver() {  
+		 System.setProperty("webdriver.chrome.driver", "E:\\drivers\\chromedriver_win32\\chromedriver.exe");
+		  driver = new ChromeDriver();
+	  } 
+
+	@Test
+	public void test ()
+	{
+		
+		 driver.get("http://google.com");  
+		 driver.findElement(By.name("q")).sendKeys("selenium");
+		 driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		 driver.quit();
+		
+		
+	}
+}
